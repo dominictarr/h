1d23a2af84fc7d980dd2ae8bf16db39db481b874
Dominic Tarr <dominic.tarr@gmail.com>
2019-07-19 21:43:27 +0300

    repurpose

--- index.js
+++ index.js
@@ -1,63 +1,92 @@
-;(function () {
+var escape = require('html-escape')
+var cssesc = require('cssesc')
 
-function h() {
-  var args = [].slice.call(arguments), e = null
-  function item (l) {
-    
-    function parseClass (string) {
-      var m = string.split(/([\.#]?[a-zA-Z0-9_-]+)/)
-      m.forEach(function (v) {
-        var s = v.substring(1,v.length)
-        if(!v) return 
-        if(!e)
-          e = document.createElement(v)
-        else if (v[0] === '.')
-          e.classList.add(s)
-        else if (v[0] === '#')
-          e.setAttribute('id', s)
-        
-      })
-    }
-
-    if(l == null)
-      ;
-    else if('string' === typeof l) {
-      if(!e)
-        parseClass(l)
-      else
-        e.appendChild(document.createTextNode(l))
-    }
-    else if('number' === typeof l 
-      || 'boolean' === typeof l
-      || l instanceof Date 
-      || l instanceof RegExp ) {
-        e.appendChild(document.createTextNode(l.toString()))
-    }
-    else if (Array.isArray(l))
-      l.forEach(item)
-    else if(l instanceof HTMLElement)
-      e.appendChild(l)
-    else if ('object' === typeof l) {
-      for (var k in l) {
-        if('function' === typeof l[k])
-          e.addEventListener(k, l[k])
-        else if(k === 'style') {
-          for (var s in l[k])
-            e.style.setProperty(s, l[k][s])
-        }
-        else
-          e.setAttribute(k, l[k])
-      }
-    }
-  }
-  while(args.length) {
-    item(args.shift())
-  }
-  return e
+function isString(s) {
+  return 'string' === typeof s
 }
 
-if(typeof module === 'object')
-  module.exports = h
-else
-  this.h = h
-})()
+var rx_tagname = /^[a-zA-Z]+(?:[\-a-zA-Z0-9])*$/
+var rx_tagname = /^[a-zA-Z]+(?:[\-a-zA-Z0-9])*$/
+var rx_tagname_with_class = /^[a-zA-Z]+(?:[\-a-zA-Z0-9])*([\.#][^\s#.]+)+$/
+
+function isName(t) {
+  return isString(t) && rx_tagname.test(t)
+}
+
+function isNameWithClass (n) {
+  return isString(n) && rx_tagname_with_class.test(n)
+}
+
+var isArray = Array.isArray
+
+function isObject (o) {
+  return o && 'object' == typeof o && !isArray(o)
+}
+
+module.exports = function toHTML (ary) {
+  if(isString(ary)) return escape(ary)
+  var str = '', innerHTML
+  if(isArray(ary) && ary.every(isArray)) {
+    return ary.map(toHTML).join('')
+  }
+  if(isArray(ary)) {
+    var i = 1, tag, attrs, innerHTML
+    if(isName(ary[0]))
+      var tag = ary[0]
+    else if(isNameWithClass(ary[0])) {
+      var parts = ary[0].split(/([\.#])/g)
+      var tag = parts.shift()
+      var id = '', _class = ''
+      for(var j = 0; j < parts.length; j+=2)
+        if(parts[j] === '.')
+          _class = (_class +' '+cssesc(parts[j+1], {isIdentifier: true})).trim()
+        else if(parts[j] === '#')
+          id = cssesc(parts[j+1], {isIdentifier: true})
+    }
+    else
+      throw new Error('expected tag name, got:'+ary[0])
+
+    if(isObject(ary[1])) {
+      attrs = ary[1]
+      if(attrs.className) {
+        _class = attrs.className
+        delete attrs.className
+      }
+      i = 2
+    }
+    str = '<'+tag
+
+    if(_class) str += ' class="'+_class+'"'
+    if(id) str += ' id="'+id+'"'
+
+    console.log("ARY", isObject(ary[1]), ary, attrs, innerHTML)
+    if(!attrs) str += '>'
+    else if(attrs.innerHTML) {
+      str +='>'
+      innerHTML = attrs.innerHTML
+      delete attrs.innerHTML
+    }
+    else {
+      for(var k in attrs) {
+        if(!isName(k)) throw new Error('attribute key not a name:'+k)
+        if(attrs[k] === true)
+          str += ' ' + k
+        else if(attrs[k] != null)
+          str += ' ' + k + '="' + escape(attrs[k]) + '"'
+      }
+      str += '>'
+    }
+    if(innerHTML)
+      str += innerHTML //CAUTION: explicitly not escaped.
+    else
+      for(;i < ary.length; i++) {
+        if(isArray(ary[i]) && isArray(ary[i][0]))
+          str += ary[i].map(toHTML).join('')
+        else
+          str += toHTML(ary[i])
+      }
+    str += '</'+tag+'>'
+    return str
+  }
+  else return ''
+}
